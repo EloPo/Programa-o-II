aa2a7f6ff329ff81a662db9c4f0681e9a48bd6d4
Eloisa Potrich <30781570+EloPo@users.noreply.github.com>
2019-06-09 20:38:15 -0300

    Add files via upload

---
+++ Aep22/Aep22/Aep22/src/main/java/Aep22/Aep22/App.java
@@ -0,0 +1,7 @@
+package Aep22.Aep22;
+
+public class App {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ Aep22/Aep22/Aep22/src/main/java/Aep22/Aep22/CriarConexao.java
@@ -0,0 +1,18 @@
+package Aep22.Aep22;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class CriarConexao {
+	private CriarConexao(){
+		
+	}
+	public static Connection conexao() throws Exception {
+		Connection conn = DriverManager.getConnection(
+				"jdbc:postgresql://localhost:5432/3esoft2019",
+				"postgres",
+				"unicesumar");
+		return conn;
+	}
+}
